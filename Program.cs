2a311686b352e5150ffd15156d413e46dd2f9890
Yosri-ctrl <yosribouabid@gmail.com>
2023-08-06 17:24:23 +0100

    implementing /games/{id}

--- Program.cs
+++ Program.cs
@@ -33,5 +33,15 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/games", () => games);
+app.MapGet("/games/{id}", (int id) =>
+{
+    Game? game = games.Find(game => game.Id == id);
+
+    if (game is null) {
+        return Results.NotFound("");
+    }
+
+    return Results.Ok(game);
+});
 
 app.Run();
